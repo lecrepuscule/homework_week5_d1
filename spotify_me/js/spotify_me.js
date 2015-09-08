5c2ba1c5d4e948dc7658922405d83ef4e3b96ce1
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-08 08:13:54 +0100

    preview audio feature added

--- spotify_me/js/spotify_me.js
+++ spotify_me/js/spotify_me.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
   var submitButton = $("#submit-button");
   var searchString = $("#search-string");
   var searchType = $("#search-type");
-  var results = $(".results");
+  var resultsDisplay = $(".results-display");
+  var audioResults = $(".audio-results");
 
   submitButton.on("click", search);
 
@@ -11,17 +12,42 @@ $(document).ready(function(){
     e.preventDefault();
     var searchString = $("#search-string").val();
     var searchType = $("#search-type").val();
-    resultType = searchType + "s";
+    results = searchType + "s";
 
     var url = "https://api.spotify.com/v1/search?q="+ searchString + "&type=" + searchType;
 
     $.get(url, function(response){
-      results.empty();
-      $.each(response[resultType].items, function(index, resultType){
-        var result = $("<div class='results'>" + resultType.name + "</div>");
-        results.append(result);
+      resultsDisplay.empty();
+      $.each(response[results].items, function(index, resultType){
+        var result = $("<div class='result'>" + resultType.name + "</div>");
+        resultsDisplay.append(result);
+        if (resultType.type === "track") {
+          var track = $("<audio src='" + resultType.preview_url + "' preload='auto' controls>" +resultType.name + "</audio>");
+          resultsDisplay.append(track);
+        }
+       
       })
     })
+
+    // $.ajax({
+    //   url: url,
+    //   beforeSend: function(xhr){
+    //     xhr.setRequestHeader("Authorization","Bearer BQA7Qs_XAMnpqs21aWSC-DMXg5RZf629QiiE8cYtmkLY7nOvG3NlVvPHOo41Zw39sJXkSsHFqjrEH017hY1X9STsud19GWRin-fqm8nHxEwGbE3bTObpE7d-ZrW0cMdraRQ48lRaX4cwUkk");
+    //   },
+    //   success: function(response){
+    //     console.log(response);
+    //     resultsDisplay.empty();
+    //     audioResults.empty();
+    //     $.each(response[results].items, function(index, resultType){
+    //       var result = $("<div class='results'>" + resultType.name + "</div>");
+    //       resultsDisplay.append(result);
+    //       if (resultType.type === "track") {
+    //         var track = $("<audio src='" + resultType.preview_url + "' preload='auto' controls>" +resultType.name + "</audio>");
+    //         audioResults.append(track);
+    //       }
+    //     })
+    //   }
+    // });    
   }
 
 })
